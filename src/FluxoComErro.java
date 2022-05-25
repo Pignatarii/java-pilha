45a08ff2d150496dee9f7295662513d75b9f12b0
Pignatarii <alexandre.a.pignatari@outlook.com>
2022-05-25 16:33:28 -0300

    A5-E1

--- src/FluxoComErro.java
+++ src/FluxoComErro.java
@@ -1,14 +1,14 @@
-public class FluxoComError {
+public class FluxoComErro {
 
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		try{
 		    metodo1();
-		} catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
+		} catch(ArithmeticException | NullPointerException ex) {
 		    String msg = ex.getMessage();
 		    System.out.println("Exception " + msg);
 		    ex.printStackTrace();
-		} 
+		}
 		System.out.println("Fim do main");
 	}
 
